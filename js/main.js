7bb30db5ef04793051fb6f35a2ac8d22b5b6a4b8
Sergey <sergey.a.shagaev@mail.ru>
2019-02-15 00:19:30 +0400

    Урок №3

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var cart = {} //корзина
 function init() {
 	//вычитуем файл goods.json
 	$.getJSON("goods.json", goodsOut);
@@ -19,10 +20,32 @@ function goodsOut(data) {
 		out +=`<p class="name">${data[key].name}</p>`;
 		out +=`<img src="images/${data[key].img}" alt="">`;
 		out +=`<div class="cost">${data[key].cost}</div>`;
-		out +='<button class="add-to-cart">Купить</button>';
+		out +=`<button class="add-to-cart" data-id="${key}">Купить</button>`;
 		out +='</div>';
 	}
-	$('.goods-out').html(out);
+	$('.goods-out').html(out); 
+	$('.add-to-cart').on('click', addToCart);
+}
+
+function addToCart() {
+	//добавляем товар в корзину
+	var id = $(this).attr('data-id');
+	//console.log(id);
+	if (cart[id] == undefined) {
+		cart[id] = 1;
+	}
+	else {
+		cart[id]++;
+	}
+	showMiniCart();
+}
+
+function showMiniCart() {
+	var out="";
+	for (var key in cart) {
+		out += key + '----' + cart[key]+'<br>';
+	}
+	$('.mini-cart').html(out);
 }
 
 $(document).ready(function () {
