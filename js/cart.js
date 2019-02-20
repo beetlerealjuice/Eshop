2dc2d7eda26d86a26c93aba7b197bc49b7dd6eb3
Sergey <sergey.a.shagaev@mail.ru>
2019-02-21 00:34:44 +0400

    Урок №6

--- js/cart.js
+++ js/cart.js
@@ -21,11 +21,16 @@ function showCart() {
 				out += `<button data-id="${id}" class="del-goods">x</button>`;
 				out += `<img src="images\\${goods[id].img}">`;
 				out += `  ${goods[id].name  }`;
-				out += `  ${cart[id]  }`;
+				out += `  <button data-id="${id}" class="minus-goods">-</button>  `;
+				out += `  ${cart[id]}  `;
+				out += `  <button data-id="${id}" class="plus-goods">+</button>  `;
+				out += cart[id]*goods[id].cost;
 				out += `<br>`;
 			}
 			$('.main-cart').html(out); 
-			$('.del-goods').on('click', delGoods)
+			$('.del-goods').on('click', delGoods);
+			$('.plus-goods').on('click', plusGoods);
+			$('.minus-goods').on('click', minusGoods);
 		});
 	}	
 }
@@ -34,6 +39,26 @@ function delGoods() {
 	var id = $(this).attr('data-id');
 	console.log(id);
 	delete cart[id];
+	saveCart();
+	showCart();
+}
+
+function plusGoods() {
+	var id = $(this).attr('data-id');
+	cart[id]++;
+	saveCart();
+	showCart();
+}
+
+function minusGoods() {
+	var id = $(this).attr('data-id');
+	if (cart[id]==1) {
+	delete cart[id];
+	}
+	else {
+		cart[id]--;	
+	}
+	
 	saveCart();
 	showCart();
 }
@@ -48,7 +73,35 @@ function isEmpty(object) {
     return false;
 }
 
+function sendEmail() {
+	var ename = $('#ename').val();
+	var email = $('#email').val();
+	var ephone = $('#ephone').val();
+	if (ename!='' && email!='' && ephone!='') {
+		if (isEmpty(cart)) {
+			$.post(
+				"core/mail.php",
+				{
+					"ename" : ename,
+					"email" : email,
+					"ephone" : ephone,
+					"cart" : cart
+				},
+				function(data) {
+					console.log(data);
+				}
+			);
+		}
+		else {
+			alert('Корзина пуста!')
+		}
+	}
+	else {
+		alert('Заполните поля');
+	}
+}
 
 $(document).ready(function () {
 	loadCart();
+	$('.send-email').on('click', sendEmail);
 });
